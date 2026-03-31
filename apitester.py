4d9ca6ee4a17a8d59548a783862bef042bd897ec
Raphaëlle Christmann <raphaelle.christmann@etu.minesparis.psl.eu>
2026-03-31 17:02:18 +0200

    tpppppppppppppppp

--- apitester.py
+++ apitester.py
@@ -31,7 +31,24 @@ data = Path(__file__).parent.absolute() / 'data'
 associations_df = pd.read_csv(data / 'associations_etudiantes.csv')
 evenements_df = pd.read_csv(data / 'evenements_associations.csv')
 
-# -----------------
-## your code (route handlers) goes here
-# -----------------
+@app.get("/api/alive")
+def is_alive():
+    return {"message" : "Alive"}
 
+@app.get("/api/associations")
+def get_associations():
+    names = associations_df["nom"].tolist()
+    return(names)
+
+@app.get("/api/evenements")
+def get_evenements():
+    events = evenements_df["nom"].tolist()
+    return events
+
+@app.get("/api/association/{association_name}")
+def get_association_details(association_name:str):
+    assoc_details = associations_df[associations_df["nom"] == association_name]
+    if assoc_details.empty:
+        raise HTTPException(status_code=404, detail = "Association not found")
+    else:
+        return assoc_details.to_dict(orient="records")[0]
